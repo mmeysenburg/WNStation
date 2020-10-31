a9a0ec0d55e2ac990da590f7922175e032e3deaa
Mark Meysenburg <mark.meysenburg@doane.edu>
2020-10-31 18:30:15 -0500

    first working EPD version.

--- TKTest.py
+++ TKTest.py
@@ -5,6 +5,17 @@ from model import NewsAPIReader, NWSReader
 import netifaces as ni
 import datetime
 
+libDir = '/home/pi/e-Paper/RaspberryPi&JetsonNano/python/lib'
+
+import os
+import sys
+
+if os.path.exists(libDir):
+    sys.path.append(libDir)
+
+from waveshare_epd import epd7in5b_HD
+
+
 def formatDisplayText(text, maxWidth, font):
     """Format a string as a pseudo-justified text for the display.
 
@@ -76,78 +87,86 @@ def formatDisplayText(text, maxWidth, font):
     return (hl, height)
         
 
-# constants for positioning content
-BASE_HEADLINE_Y = 70
-COL_1_X = 5
-COL_2_X = 298
-COL_3_X = 592
-NEWS_WIDTH = 280
+epd = epd7in5b_HD.EPD()
+epd.init()
+epd.Clear()
 
-# fonts for display
-headlineFont = ImageFont.truetype('./times.ttf', 22)
-newsFont = ImageFont.truetype('./times.ttf', 18)
+while True:
+    # constants for positioning content
+    BASE_HEADLINE_Y = 70
+    COL_1_X = 5
+    COL_2_X = 298
+    COL_3_X = 592
+    NEWS_WIDTH = 280
 
-# get three random headlines
-napir = NewsAPIReader.NewsAPIReader('ae277ae39eb84b0eb01efeae417fe724')
-headlines = napir.getRandomHeadlines(3)
+    # fonts for display
+    headlineFont = ImageFont.truetype('./times.ttf', 22)
+    newsFont = ImageFont.truetype('./times.ttf', 18)
 
-# get weather info
-NWSStation = 'KLNK'
-NWSOffice = 'OAX'
-NWSGridX = '47'
-NWSGridY = '30'
-nwsr = NWSReader.NWSReader(NWSOffice, NWSGridX, NWSGridY, NWSStation)
-currentConditions = nwsr.getCurrentConditions()
-forecast = nwsr.getForecast()
-print(forecast)
+    # get three random headlines
+    napir = NewsAPIReader.NewsAPIReader('ae277ae39eb84b0eb01efeae417fe724')
+    headlines = napir.getRandomHeadlines(3)
 
-# look up wifi address of pi via ifconfig   
-#ni.ifaddresses('wlp3s0')
-#ni.ifaddresses('BD743578-8982-4E1A-987C-6175649CE971')
-ipAddress = '192.168.7.7'#ni.ifaddresses('eth1')[ni.AF_INET][0]['addr']
+    # get weather info
+    NWSStation = 'KLNK'
+    NWSOffice = 'OAX'
+    NWSGridX = '47'
+    NWSGridY = '30'
+    nwsr = NWSReader.NWSReader(NWSOffice, NWSGridX, NWSGridY, NWSStation)
+    currentConditions = nwsr.getCurrentConditions()
+    forecast = nwsr.getForecast()
 
-# create black layer for composite image
-blackLayer = Image.open('blackLayer.bmp')
-blackLayer.load()
-blackLayerDraw = ImageDraw.Draw(blackLayer)
+    # look up wifi address of pi via ifconfig   
+    ipAddress = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
 
-# first news item
-(hl, height) = formatDisplayText(headlines[0][0], NEWS_WIDTH, headlineFont)
-blackLayerDraw.text((COL_1_X, BASE_HEADLINE_Y), hl, font=headlineFont)
-(text, textHeight) = formatDisplayText(headlines[0][1], NEWS_WIDTH, newsFont)
-blackLayerDraw.text((COL_1_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
+    # create black layer for composite image
+    blackLayer = Image.open('blackLayer.bmp')
+    blackLayer.load()
+    blackLayerDraw = ImageDraw.Draw(blackLayer)
 
-# second news item
-(hl, height) = formatDisplayText(headlines[1][0], NEWS_WIDTH, headlineFont)
-blackLayerDraw.text((COL_2_X, BASE_HEADLINE_Y), hl, font=headlineFont)
-(text, textHeight) = formatDisplayText(headlines[1][1], NEWS_WIDTH, newsFont)
-blackLayerDraw.text((COL_2_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
+    # first news item
+    (hl, height) = formatDisplayText(headlines[0][0], NEWS_WIDTH, headlineFont)
+    blackLayerDraw.text((COL_1_X, BASE_HEADLINE_Y), hl, font=headlineFont)
+    (text, textHeight) = formatDisplayText(headlines[0][1], NEWS_WIDTH, newsFont)
+    blackLayerDraw.text((COL_1_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
 
-# third news item
-(hl, height) = formatDisplayText(headlines[2][0], NEWS_WIDTH, headlineFont)
-blackLayerDraw.text((COL_3_X, BASE_HEADLINE_Y), hl, font=headlineFont)
-(text, textHeight) = formatDisplayText(headlines[2][1], NEWS_WIDTH, newsFont)
-blackLayerDraw.text((COL_3_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
+    # second news item
+    (hl, height) = formatDisplayText(headlines[1][0], NEWS_WIDTH, headlineFont)
+    blackLayerDraw.text((COL_2_X, BASE_HEADLINE_Y), hl, font=headlineFont)
+    (text, textHeight) = formatDisplayText(headlines[1][1], NEWS_WIDTH, newsFont)
+    blackLayerDraw.text((COL_2_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
 
-# create red layer for composite image
-redLayer = Image.open('redLayer.bmp')
-redLayer.load()
-redLayerDraw = ImageDraw.Draw(redLayer)
+    # third news item
+    (hl, height) = formatDisplayText(headlines[2][0], NEWS_WIDTH, headlineFont)
+    blackLayerDraw.text((COL_3_X, BASE_HEADLINE_Y), hl, font=headlineFont)
+    (text, textHeight) = formatDisplayText(headlines[2][1], NEWS_WIDTH, newsFont)
+    blackLayerDraw.text((COL_3_X, BASE_HEADLINE_Y + height + 5), text, font=newsFont)
 
-# IP address to allow VNC access
-redLayerDraw.text((5, 505), ipAddress, font=newsFont)
+    # create red layer for composite image
+    redLayer = Image.open('redLayer.bmp')
+    redLayer.load()
+    redLayerDraw = ImageDraw.Draw(redLayer)
 
-# day, date
-today = datetime.datetime.today()
-dayDate = today.strftime('%a, %d %b %y')
-redLayerDraw.text((10, 7), dayDate, font=newsFont)
+    # IP address to allow VNC access
+    redLayerDraw.text((5, 505), ipAddress, font=newsFont)
 
-# time
-currTime = today.strftime('%H:%M:%S')
-redLayerDraw.text((790, 505), currTime, font=newsFont)
+    # day, date
+    today = datetime.datetime.today()
+    dayDate = today.strftime('%a, %d %b %y')
+    redLayerDraw.text((10, 7), dayDate, font=newsFont)
 
-# current temperature / conditions
-redLayerDraw.text((665, 5), currentConditions['temp'] + ' ' + currentConditions['text'], font=newsFont)
+    # time
+    currTime = today.strftime('%H:%M:%S')
+    redLayerDraw.text((790, 505), currTime, font=newsFont)
 
-blackLayer.show()
-redLayer.show()
+    # current temperature / conditions
+    redLayerDraw.text((670, 5), currentConditions['temp'] + ' ' + currentConditions['text'], font=newsFont)
+
+    # push to display
+    epd.display(epd.getbuffer(blackLayer) ,epd.getbuffer(redLayer))
+    
+    import time
+    epd.sleep()
+    time.sleep(60*10)
+    
+    epd.Clear()
